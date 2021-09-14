56bbc821e3980e3c52aa75edc384040aa51f8b2e
Andrej Bilec <andrej.bilec@gmail.com>
2021-09-14 15:04:11 +0200

    New: Basic bubble chart with D3 added

---
+++ src/components/BubbleChartD3.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Bubble chart d3</h1>
+    <div id="chart" />
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  mounted () {
+    const svg = d3.select('#chart').append('svg')
+      .attr('width', 1200)
+      .attr('height', 1200)
+
+    svg.selectAll('circle')
+      .data(this.data).enter()
+      .append('circle')
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('r', d => d.val)
+      .attr('fill', '#69b3a2')
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
-    Visualisation Library Demo
+    <BubbleChartD3 :data="data" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import BubbleChartD3 from '@/components/BubbleChartD3.vue'
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  components: { BubbleChartD3 },
+  data () {
+    return {
+      data: [
+        { x: 100, y: 60, val: 10 },
+        { x: 30, y: 80, val: 20 },
+        { x: 50, y: 40, val: 12 },
+        { x: 190, y: 100, val: 40 },
+        { x: 80, y: 170, val: 20 }
+      ]
+    }
+  }
 })
 </script>
