077f52a30cf7f434a707e5b297d9ab53399e839c
Andrej Bilec <andrej.bilec@gmail.com>
2021-09-16 11:27:08 +0200

    New: Progress with canvas chart

--- src/components/BubbleChartD3.vue
+++ src/components/BubbleChartD3.vue
@@ -19,15 +19,6 @@ export default {
     const chartHeight = 700
     const defaultZoom = 1
 
-    const svg = d3.select('#chart').append('svg')
-      .attr('width', chartWidth)
-      .attr('height', chartHeight)
-
-    svg.append('rect')
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .attr('fill', '#EEE')
-
     const coordinateScaleX = d3.scaleLinear()
     const coordinateScaleY = d3.scaleLinear()
     const radiusScaleArticle = d3.scaleLinear()
@@ -36,22 +27,53 @@ export default {
     coordinateScaleY.domain([-200, 200]).range([0, chartHeight * defaultZoom])
     radiusScaleArticle.domain([0, 10]).range([0, 3])
 
-    svg.selectAll('circle')
-      .data(this.data).enter()
-      .append('circle')
-      .attr('cx', d => coordinateScaleX(d.x))
-      .attr('cy', d => coordinateScaleY(d.y))
-      .attr('r', d => d.pointData._type === 'ARTICLE' ? radiusScaleArticle(d.pointData.engagement.overallScore) : 20)
-      .attr('fill', d => d.pointData._type === 'ARTICLE' ? '#69b3a2' : 'gray')
-      .attr('cursor', 'pointer')
-      .attr('data-type', d => d.pointData._type)
-      .attr('data-id', d => d.pointData.id)
+    const canvasChart = d3.select('#chart').append('canvas')
+      .attr('width', chartWidth)
+      .attr('height', chartHeight)
 
-    svg.selectAll('circle').on('click', (d) => {
-      alert(`${d.target.getAttribute('data-type')} ${d.target.getAttribute('data-id')}`)
-    })
+    const context = canvasChart.node().getContext('2d')
 
-    svg.call(d3.zoom().on('zoom', (e) => d3.selectAll('#chart circle').attr('transform', e.transform)))
+    context.fillStyle = '#EEE'
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+
+    for (const point of this.data) {
+      context.beginPath()
+      context.fillStyle = point.pointData._type === 'ARTICLE' ? '#69b3a2' : 'gray'
+
+      const px = coordinateScaleX(point.x)
+      const py = coordinateScaleY(point.y)
+      const r = point.pointData._type === 'ARTICLE' ? radiusScaleArticle(point.pointData.engagement.overallScore) : 20
+
+      context.arc(px, py, r, 0, 2 * Math.PI, true)
+      context.closePath()
+      context.fill()
+    }
+
+    // const svg = d3.select('#chart').append('svg')
+    //   .attr('width', chartWidth)
+    //   .attr('height', chartHeight)
+
+    // svg.append('rect')
+    //   .attr('width', '100%')
+    //   .attr('height', '100%')
+    //   .attr('fill', '#EEE')
+
+    // svg.selectAll('circle')
+    //   .data(this.data).enter()
+    //   .append('circle')
+    //   .attr('cx', d => coordinateScaleX(d.x))
+    //   .attr('cy', d => coordinateScaleY(d.y))
+    //   .attr('r', d => d.pointData._type === 'ARTICLE' ? radiusScaleArticle(d.pointData.engagement.overallScore) : 20)
+    //   .attr('fill', d => d.pointData._type === 'ARTICLE' ? '#69b3a2' : 'gray')
+    //   .attr('cursor', 'pointer')
+    //   .attr('data-type', d => d.pointData._type)
+    //   .attr('data-id', d => d.pointData.id)
+    //
+    // svg.selectAll('circle').on('click', (d) => {
+    //   alert(`${d.target.getAttribute('data-type')} ${d.target.getAttribute('data-id')}`)
+    // })
+
+    // svg.call(d3.zoom().on('zoom', (e) => d3.selectAll('#chart circle').attr('transform', e.transform)))
   }
 }
 </script>
